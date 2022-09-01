1c8df79a4e4337dceea95320f9c266b6b46fad00
riyadelberkawy <riyad.elberkawy@gmail.com>
2022-09-01 17:47:56 +0200

    build server side

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,18 +4,56 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+const fs_1 = __importDefault(require("fs"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const port = 5555;
-app.get("/", (req, res) => {
-    const fs = require("fs");
-    const data = fs.readFileSync("./TestData.json", "utf8");
-    const json = JSON.parse(data);
-    res.send(json);
-    //   res.send("Express + TypeScript Server");
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.get("/", (_, res) => {
+    res.send("Hello World!");
 });
-// read data.json file via fs module
+// words endpoint that returns 10 random words from the list of words
+app.get("/words", (_, res) => {
+    const data = getDate();
+    const words = data.wordList;
+    const randomWords = [];
+    for (let i = 0; i < 10; i++) {
+        const randomIndex = Math.floor(Math.random() * words.length);
+        // check if the word is already in the array
+        if (randomWords.includes(words[randomIndex]))
+            i--;
+        else
+            randomWords.push(words[randomIndex]);
+    }
+    res.send(randomWords);
+});
+// endpoint that calculate the rank of the score
+app.post("/rank", (req, res) => {
+    const data = getDate();
+    const scores = data.scoresList;
+    const score = req.body.score;
+    // return bad request if score is provided as a number
+    if (typeof score !== "number")
+        return res.status(400).send("No score provided");
+    let rank = 0;
+    // loop through the scores and find the rank of the score
+    for (let i = 0; i < scores.length; i++) {
+        if (scores[i] < score) {
+            rank++;
+        }
+    }
+    // round the rank to the nearest hundredth.
+    rank = (rank / scores.length) * 100;
+    rank = Math.round(rank * 100) / 100;
+    return res.json({ rank });
+});
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}/`);
 });
+// utlity function to get the data from the json file
+const getDate = () => {
+    const data = fs_1.default.readFileSync("./TestData.json", "utf8");
+    const json = JSON.parse(data);
+    return json;
+};
